6757ff17aa2bfdef50d7979e438944afb4705669
Peter <peter@MacBook-Pro-uzivatela-Peter.local>
2025-03-12 18:02:11 +0100

    Added project

---
+++ SpeceApp/SpeceApp/SpeceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeceAppApp.swift
+//  SpeceApp
+//
+//  Created by Peter on 12/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SpeceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
